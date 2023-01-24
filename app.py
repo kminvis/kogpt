a211a260c482dfcb63f9f8757d825643ed28f4c1
Minseok_Kim <114811507+kminvis@users.noreply.github.com>
2023-01-25 00:22:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,20 @@ from streamlit.components.v1 import html
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM 
 
-tokenizer = AutoTokenizer.from_pretrained(
-  'kakaobrain/kogpt', revision='KoGPT6B-ryan1.5b-float16',  # or float32 version: revision=KoGPT6B-ryan1.5b
-  bos_token='[BOS]', eos_token='[EOS]', unk_token='[UNK]', pad_token='[PAD]', mask_token='[MASK]'
-)
-model = AutoModelForCausalLM.from_pretrained(
-  'kakaobrain/kogpt', revision='KoGPT6B-ryan1.5b-float16',  # or float32 version: revision=KoGPT6B-ryan1.5b
-  pad_token_id=tokenizer.eos_token_id,
-  torch_dtype='auto', low_cpu_mem_usage=True
-).to(device='cuda', non_blocking=True)
-_ = model.eval()
+
 
 def on_button_click():
     try:
+        tokenizer = AutoTokenizer.from_pretrained(
+          'kakaobrain/kogpt', revision='KoGPT6B-ryan1.5b-float16',  # or float32 version: revision=KoGPT6B-ryan1.5b
+          bos_token='[BOS]', eos_token='[EOS]', unk_token='[UNK]', pad_token='[PAD]', mask_token='[MASK]'
+        )
+        model = AutoModelForCausalLM.from_pretrained(
+          'kakaobrain/kogpt', revision='KoGPT6B-ryan1.5b-float16',  # or float32 version: revision=KoGPT6B-ryan1.5b
+          pad_token_id=tokenizer.eos_token_id,
+          torch_dtype='auto', low_cpu_mem_usage=True
+        ).to(device='cuda', non_blocking=True)
+        _ = model.eval()
         prompt = '인간처럼 생각하고, 행동하는 \'지능\'을 통해 인류가 이제까지 풀지 못했던'
         with torch.no_grad():
           tokens = tokenizer.encode(prompt, return_tensors='pt').to(device='cuda', non_blocking=True)
