9493aed263e9493ac8ea28198d2a4313774af838
Minseok_Kim <114811507+kminvis@users.noreply.github.com>
2023-01-25 00:22:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,12 +16,15 @@ model = AutoModelForCausalLM.from_pretrained(
 _ = model.eval()
 
 def on_button_click():
-    prompt = '인간처럼 생각하고, 행동하는 \'지능\'을 통해 인류가 이제까지 풀지 못했던'
-    with torch.no_grad():
-      tokens = tokenizer.encode(prompt, return_tensors='pt').to(device='cuda', non_blocking=True)
-      gen_tokens = model.generate(tokens, do_sample=True, temperature=0.8, max_length=64)
-      generated = tokenizer.batch_decode(gen_tokens)[0]
+    try:
+        prompt = '인간처럼 생각하고, 행동하는 \'지능\'을 통해 인류가 이제까지 풀지 못했던'
+        with torch.no_grad():
+          tokens = tokenizer.encode(prompt, return_tensors='pt').to(device='cuda', non_blocking=True)
+          gen_tokens = model.generate(tokens, do_sample=True, temperature=0.8, max_length=64)
+          generated = tokenizer.batch_decode(gen_tokens)[0]
 
-    st.write(generated)
-
+        st.write(generated)
+    except:
+        st.write("다시 시도해 주십시오")
+        
 st.button("예측확인", on_click=on_button_click)
